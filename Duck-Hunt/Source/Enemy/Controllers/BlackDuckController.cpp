2ff0ab9d88823f41551114a68f134389d21561bf
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-06-27 12:16:49 +0530

    experimenting with movement

--- Duck-Hunt/Source/Enemy/Controllers/BlackDuckController.cpp
+++ Duck-Hunt/Source/Enemy/Controllers/BlackDuckController.cpp
@@ -1,9 +1,14 @@
 #include "Enemy/Controllers/BlackDuckController.h"
+#include "Enemy/EnemyModel.h"
+
+#include "Global/ServiceLocator.h"
 
 namespace Enemy
 {
 	namespace Controller
 	{
+		using namespace Global;
+
 		BlackDuckController::BlackDuckController(EnemyType type) : EnemyController(type)
 		{
 			
@@ -17,11 +22,42 @@ namespace Enemy
 		void BlackDuckController::initialize()
 		{
 			EnemyController::initialize();
+
+			movement_direction = enemy_model->getMovementDirection();
 		}
 
 		void BlackDuckController::move()
 		{
+			switch (movement_direction)
+			{
+			case MovementDirection::LEFT_DIAGONAL:
+				moveLeftDiagonal();
+				break;
 
+			case MovementDirection::RIGHT_DIAGONAL:
+				moveRightDiagonal();
+				break;
+			}
+		}
+
+		void BlackDuckController::moveLeftDiagonal()
+		{
+			sf::Vector2f current_position = enemy_model->getEnemyPosition();
+
+			current_position.x -= enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+			current_position.y += enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+
+			enemy_model->setEnemyPosition(current_position);
+		}
+
+		void BlackDuckController::moveRightDiagonal()
+		{
+			sf::Vector2f current_position = enemy_model->getEnemyPosition();
+
+			current_position.x += enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+			current_position.y += enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+
+			enemy_model->setEnemyPosition(current_position);
 		}
 	}
 }
