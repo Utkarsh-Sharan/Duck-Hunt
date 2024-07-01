d73886dfa101b18f3cdc7ebd5768f11e9fced911
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-07-01 11:29:36 +0530

    created player service

---
+++ Duck-Hunt/Header/Player/PlayerService.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace Player
+{
+	class PlayerController;
+
+	class PlayerService
+	{
+	private:
+		PlayerController* player_controller;
+
+	public:
+		PlayerService();
+		~PlayerService();
+
+		void initialize();
+		void update();
+		void render();
+
+		void increaseEnemiesKilled(int val);
+
+		void reset();
+	};
+}
---
+++ Duck-Hunt/Source/Player/PlayerService.cpp
@@ -0,0 +1,45 @@
+#include "Player/PlayerService.h"
+#include "Global/ServiceLocator.h"
+#include "Player/PlayerController.h"
+
+namespace Player
+{
+	using namespace Global;
+
+	PlayerService::PlayerService()
+	{
+		player_controller = new PlayerController();
+	}
+
+	PlayerService::~PlayerService()
+	{
+		delete(player_controller);
+
+		player_controller = nullptr;
+	}
+
+	void PlayerService::initialize()
+	{
+		player_controller->initialize();
+	}
+
+	void PlayerService::update()
+	{
+		player_controller->update();
+	}
+
+	void PlayerService::render()
+	{
+		player_controller->render();
+	}
+
+	void PlayerService::increaseEnemiesKilled(int val)
+	{
+		player_controller->increaseEnemiesKilled(val);
+	}
+
+	void PlayerService::reset()
+	{
+		player_controller->reset();
+	}
+}
